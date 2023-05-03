2e344f065424db7e5af1df7d41566c321451b0c0
mariabelenamat <belen.amat29@gmail.com>
2023-05-03 19:56:51 -0300

    agrego tiempo inicial

--- src/Time.gd
+++ src/Time.gd
@@ -5,7 +5,7 @@ onready var label:Label = $TimeLeft
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_label(tiempoTotal)
 
 func update_label(tiempo):
 	label.text = String(tiempo/60) + ":" + String(tiempo%60)
