5cb4da38b3288f3964c2c6dfc2dd4c194324a60f
ximena-bogado <ximenakbq@gmail.com>
2023-05-11 20:24:52 -0300

    poner pausa, sacar pausa y reiniciar el nivel

--- src/Control.gd
+++ src/Control.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 onready var pause_menu: PopupMenu = $PopupPanel
 
+signal restart_level
+
 func _ready():
 	pass # Replace with function body.
 
@@ -10,4 +12,17 @@ func _ready():
 
 
 func _on_MenuButton_pressed():
+	get_tree().paused = true
 	pause_menu.show()
+
+func _un_pause():
+	pause_menu.hide()
+	get_tree().paused = false
+
+func _on_Resume_pressed():
+	_un_pause()
+
+
+func _on_Restart_pressed():
+	_un_pause()
+	emit_signal("restart_level")
