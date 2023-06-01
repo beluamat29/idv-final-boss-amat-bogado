cce3aaa73f4fee40c0c5b2d571e57266565ab0ab
mariabelenamat <belen.amat29@gmail.com>
2023-05-31 22:30:47 -0300

    player no se puede mover hasta que la animacion del carrito no termina

--- src/entities/Child.gd
+++ src/entities/Child.gd
@@ -3,10 +3,13 @@ extends Node2D
 onready var crying = false
 onready var attention = $Attention
 onready var cryingTimer = $ChildCryingCountdown
+onready var randomTimer = $ChildRandomTimer
+
 var task_activated: bool = false
 
 signal game_finished()
 signal set_player_target()
+signal player_baby_toggle(value)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -25,5 +28,15 @@ func _on_Stroller_input_event(viewport, event, shape_idx):
 		emit_signal("set_player_target")
 
 func _on_Stroller_body_entered(body):
-	if task_activated:
-		print('taking care')
+	if task_activated && crying:
+		crying = false
+		attention.visible = false
+		cryingTimer.stop()
+		randomTimer.stop()
+		emit_signal("player_baby_toggle", true)
+		$StrollerAnimation.play("swing", -1, 1)
+
+func _on_StrollerAnimation_animation_finished(anim_name):
+	emit_signal("player_baby_toggle", false)
+	randomTimer.reset()
+	
--- src/entities/ChildTimer.gd
+++ src/entities/ChildTimer.gd
@@ -10,3 +10,9 @@ func generateRandomAmountOfTime():
 	randomNumberGenerator = RandomNumberGenerator.new()
 	randomNumberGenerator.randomize()
 	return randomNumberGenerator.randf_range(5, 10)
+	
+func reset():
+	wait_time = generateRandomAmountOfTime()	
+	print(wait_time)
+	self.wait_time = wait_time
+	self.start()
