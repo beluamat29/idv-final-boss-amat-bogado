9cd018a1c6f3ab3e9bd2e8e3e9cd608d5d0a3e3b
ximena-bogado <63512143+ximena-bogado@users.noreply.github.com>
2023-06-07 23:08:15 -0300

    borrando codigo que quedo sin uso

--- src/entities/Child.gd
+++ src/entities/Child.gd
@@ -2,23 +2,20 @@ extends Node2D
 
 onready var crying = false
 onready var attention = $Attention
-onready var cryingTimer = $ChildCryingCountdown
 onready var randomTimer = $ChildRandomTimer
 
 var task_activated: bool = false
 var body_entered: bool = false
 
-signal game_finished()
 signal set_player_target()
 signal player_baby_toggle(value)
 signal crying()
-# Called when the node enters the scene tree for the first time.
+
 func _physics_process(delta):
 	if task_activated && body_entered && crying:
 		crying = false
 		attention.visible = false
 		task_activated = false
-		cryingTimer.stop()
 		randomTimer.stop()
 		emit_signal("player_baby_toggle", true)
 		$StrollerAnimation.play("swing", -1, 1)
@@ -27,10 +24,6 @@ func _on_ChildTimer_timeout():
 	attention.visible = true
 	crying = true
 	emit_signal("crying")
-	# cryingTimer.start()
-
-func _on_ChildCryingCountdown_timeout():
-	emit_signal("game_finished")
 
 func _on_Stroller_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed) && crying:
