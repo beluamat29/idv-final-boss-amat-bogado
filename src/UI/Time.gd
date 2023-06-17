1c03ddf4fe09d312fb9af821a49c0bc0fec67cba
mariabelenamat <belen.amat29@gmail.com>
2023-06-17 20:18:50 -0300

    agrego repisas y reloj funcional

--- src/UI/Time.gd
+++ src/UI/Time.gd
@@ -2,36 +2,22 @@ extends Node
 
 var tiempoTotal = 300 #segundos
 onready var label:Label = $TimeLeft
+onready var pivotHours:Position2D = $PivotHours
+onready var pivotMinutes:Position2D = $PivotMinutes
 
 signal game_over
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update_label(tiempoTotal)
-
-func update_label(tiempo):
-	var hours = tiempo/60 * 2
-	var minutes = tiempo%60
-	label.text = _hours(hours) + ":" + _minutes(minutes)
-	
-func _minutes(minutes):
-	if minutes == 0:
-		return '00'
-	if minutes <= 9:
-		return '0' + String(minutes)
-	return String(minutes)
-	
-func _hours(hours):
-	if hours == 0:
-		return '00'
-	if hours <= 9:
-		return '0' + String(hours)
-	return String(hours)
+	pass
 	
 func _on_Countdown_timeout():
 	if(tiempoTotal < 660):
+		print(tiempoTotal)
 		tiempoTotal += 5
-		update_label(tiempoTotal)
+		if(tiempoTotal%60 == 0):
+			pivotHours.rotation_degrees += 30
+		pivotMinutes.rotation_degrees += 30
 	else:
 		get_tree().paused = true
 		emit_signal("game_over")
