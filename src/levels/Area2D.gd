71db0b3732ae07a6c476e5d3727e46e22094e082
ximena-bogado <ximenakbq@gmail.com>
2023-07-02 02:15:35 -0300

    prender luz cuando se acerca

--- src/levels/Area2D.gd
+++ src/levels/Area2D.gd
@@ -3,18 +3,19 @@ extends Area2D
 onready var light: Light2D = $Light2D4
 onready var shine: Light2D = $Light2D
 var can_turn_on: bool = false
+var selected: bool = false
+var body_entered: bool = false
 
 signal _on_selected()
 
-func _on_Switch_input_event(viewport, event, shape_idx):
-	if (event is InputEventMouseButton && event.pressed):
-		if !light.is_visible_in_tree() && can_turn_on:
-			shine.hide()
-			light.show()
-		else:
-			light.hide()
-	
+func _process(delta):
+	if !light.is_visible_in_tree() && can_turn_on && selected && body_entered:
+		shine.hide()
+		light.show()
+		selected = false
 
+func _on_Switch_input_event(viewport, event, shape_idx):
+	selected = true
 
 func _on_Time_end_sunset():
 	can_turn_on = true
@@ -24,3 +25,10 @@ func _on_Time_end_sunset():
 func _on_Switch_mouse_entered():
 	if !light.is_visible_in_tree() && can_turn_on:
 		emit_signal("_on_selected")
+
+
+func _on_Switch_body_entered(body):
+	body_entered = true
+	
+func _on_Switch_body_exited(body):
+	body_entered = false
