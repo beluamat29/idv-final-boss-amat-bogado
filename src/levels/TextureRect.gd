33366634ad6402765c1df5063b07f269948794ea
ximena-bogado <63512143+ximena-bogado@users.noreply.github.com>
2023-07-13 21:20:42 -0300

    arreglo_distorsion

--- src/levels/TextureRect.gd
+++ src/levels/TextureRect.gd
@@ -8,8 +8,12 @@ func _on_StressBar_change_stress_signs(value):
 	if value > 40:
 		if value > 80:
 			material.set_shader_param("spread", value * 0.0002)
-		else:
+		elif value > 90:
+			material.set_shader_param("spread", value * 0.0005)
+		else:	
 			material.set_shader_param("spread", value * 0.0001)
+	else:
+		material.set_shader_param("spread",0)
 	
 	
 	
